a90c08eb451e33e74bebbb1c9d439f0e5247fc19
talhajilal <talhajilal@gmail.com>
2018-02-24 23:00:09 -0500

    new changein

---
+++ add_new_git_file.sh
@@ -0,0 +1,16 @@
+origin=$(git remote -v | grep push | awk '{print $2}')
+echo  "Your new changes in this directory will go to this origin $origin"
+result=$(echo $?)
+if [ "$result" -eq 0 ]; then
+echo "Proceeding to commit new file"
+else 
+echo "No git hub origin define" 
+exit
+fi
+echo "Please enter your commit msg here"
+read msg
+git init .
+git add . 
+git commit -m "$msg"
+#git push  
+git push https://talhajilal@github.com/talhajilal/hyperledger.git
